853aa94d33ac039b5d38b79101e8c7b116697cc1
manikandan-1704 <manimk1704@gmail.com>
2025-08-06 16:50:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return(
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl underline">React Admin Template</h1>
+      </div>
+  )
+}
+
+export default App;
